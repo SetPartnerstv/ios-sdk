231cc15af17005f3d60cc7cc77e866e3cac79b4c
Николай Капустин <n.kapustin@dataintegral.ru>
2023-09-14 22:34:47 +0000

    New release 2.0.0

---
+++ sdk_demo/GazpromBonusSDK/WidgetViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  GazpromBonus SDK
+//
+//  Created by SetPartnerstv on 12.09.2023.
+//
+
+import Foundation
+import GPBonusSDK
+import WebKit
+
+@objcMembers
+class WidgetViewController: SDKViewController {
+
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override open func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    override open func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+}
+
+
